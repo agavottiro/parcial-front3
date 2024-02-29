e2e181c1ed88ce6d65700423ea62a6e335fa9dc6
agavottiro <agavottiro@gmail.com>
2024-02-29 20:36:50 -0300

    Parcial listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 import { useState } from "react";
-import "./App.css";
 import Card from "./Components/Card";
-import Form from "./Components/Form";
+import './App.css'
 
 function App() {
+  const [fanatico, setFanatico] = useState({
+    name: "",
+    tvShow: "",
+  });
+  console.log(fanatico);
+  const [showCard, setShowCard] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      fanatico.name.length < 3 ||
+      fanatico.name.startsWith(" ") ||
+      fanatico.tvShow.length < 6
+    ) {
+      setError(true);
+      setShowCard(false);
+    } else {
+      setShowCard(true);
+      setError(false);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <Card />
-        <Form />
-      </div>
-    </>
+    <div>
+      <h2>¡Cuéntanos, fanático! ¿Cuál es tu serie favorita?</h2>
+      <form className="formulario" onSubmit={handleSubmit}>
+        <label>Nombre: </label>
+        <input
+          type="text"
+          onChange={(e) => setFanatico({ ...fanatico, name: e.target.value })}
+        ></input>
+        <label>Serie favorita: </label>
+        <input
+          type="text"
+          onChange={(e) => setFanatico({ ...fanatico, tvShow: e.target.value })}
+        ></input>
+        <button type="submit">Respuesta definitiva</button>
+      </form>
+      {error && <p>Por favor verifica que la información esté correcta</p>}
+      {showCard && <Card name={fanatico.name} tvShow={fanatico.tvShow} />}
+    </div>
   );
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ name, tvShow }) => {
+  return (
+    <div className="cardStyle">
+      <h4>¡Hola, {name}!</h4>
+      <h5>¡Excelente elección! A nosotros también nos encanta {tvShow}</h5>
+    </div>
+  );
 };
 
 export default Card;
